8f91964e573218fb457ad418657530ef3dc21d07
Juan Sebastian Amezquita <163927161+JuanAmezquitaAgredo@users.noreply.github.com>
2024-04-16 15:39:01 -0500

    Adjunto Workshop1

---
+++ WorkShop1/app.js
@@ -0,0 +1,59 @@
+//Worshop1//
+
+
+/**
+ * Seccion 2
+Ejercicio 4: Dentro de tu proyecto, crea un archivo app.js o index.js y añade un comentario de una sola línea y un comentario de bloque en tu archivo JavaScript que explique qué se espera en los siguientes ejercicios.
+
+Ejercicio 5: Declara una variable llamada miNumero y asígnale el valor 10. Usa console.log para imprimir tu variable en la consola.
+
+Ejercicio 6: Crea una variable llamada miBooleano con el valor true. Imprime el tipo de dato usando typeof en un console.log.
+
+Ejercicio 7: Crea una variable llamada miCadena y asígnale un valor de tu elección. Imprime la longitud de la cadena en la consola.
+
+Ejercicio 8: Crea una variable llamada miArray y asígnale un array con 3 elementos de tu elección, estos elementos deben ser string, number y array. Imprime el array en la consola.
+
+Ejercicio 9: Crea una variable llamada miObjeto y asígnale un objeto con 3 propiedades de tu elección. Imprime el objeto en la consola.
+ */
+
+// {
+// let miNumero = 10;
+// console.log(miNumero);
+
+// let miBooleano = true;
+// console.log(typeof(miBooleano));
+
+// let miCadena = "JuanSebastian";
+// console.log(miCadena.length);
+
+// let miArray = ["hola", 23, [1, 3]];
+// console.log(miArray)
+
+// let miObjeto = {
+//     nombre: "Juan",
+//     edad: 23,
+//     altura:1.83
+// };
+// console.log(miObjeto);
+// }
+
+
+//Seccion 3
+
+// nombreUsuario = prompt("ingrese su nombre");
+// alert(`Hi sr ${nombreUsuario}, welcome`)
+
+// pregunta = confirm("¿Do you like JS?");
+// if (pregunta == true){
+//     console.log("To the client liked JS")
+// }
+// else{
+//     console.log("To the client didn't liked JS")
+// }
+
+
+//seccion 4
+
+// console.warn("This is a practice");
+// console.error("la variable no esta definida");
+// console.table([1,2,3,4,5]);
